0192b3f4f5b015431d9431c660a9058924210f10
raysail <zhilongwang@foxmail.com>
2016-03-02 23:58:06 +0800

    ajax.googleapis.com's jquery is enable Invalid ，instead of code.jquery.com

--- application/views/footer.php
+++ application/views/footer.php
@@ -86,11 +86,12 @@
 <!-- Bootstrap core JavaScript
     ================================================== --> 
 <!-- Placed at the end of the document so the pages load faster --> 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>--> 
+<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script> 
 <script src="<?php echo base_url();?>design/front/js/bootstrap.min.js"></script> 
 
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug --> 
-<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+<!-- <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>-->
 
 <?php
 if($this->uri->segment(1)=='sign-up' || $this->uri->segment(1)=='update-author-profile'  || $this->uri->segment(1)=='update-editor-profile'  || $this->uri->segment(1)=='update-reviewer-profile'  || $this->uri->segment(1)=='update-publisher-profile'  )
